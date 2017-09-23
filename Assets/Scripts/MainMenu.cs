38077bd8f5176dffa0c78e8a036ed8b6faab616f
Diraj RK <diraj.ravikumar@gmail.com>
2017-09-24 00:18:48 +0800

    Added main menu and enemy health bar

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string levelToLoad = "MainLevel";
+
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+        Debug.Log("Play");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
